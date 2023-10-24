e23a4dd6e9bf5331ac4331f18fb7cac8c82d041d
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-24 18:55:58 +0100

    add class simplefield

--- Field.php
+++ Field.php
@@ -1,14 +1,16 @@
 <?php
-    class Field {
-        private $name;
-        private $type;
+    abstract class Field {
+        protected $name;
+        protected $type;
+        protected $text;
+        protected $default;
 
-        public function __construct($name, $type) {
+        public function __construct($name, $type, $text, $default = '') {
             $this->name = $name;
             $this->type = $type;
+            $this->text = $text;
+            $this->default = $default;
         }
 
-        public function render() {
-            echo "<p><label for=\"id_$this->name\">$this->name:</label><input id=\"id_$this->name\" type=\"$this->type\" name=\"$this->name\"></input></p>";
-        }
+        abstract public function render();
     }
--- testForm.php
+++ testForm.php
@@ -1,12 +1,13 @@
 <?php 
     require_once 'Form.php';
     require_once 'Field.php';
+    require_once 'SimpleField.php';
     $form1 = new Form("validate.php", "Prueba formulario", "get");
-    $field1 = new Field("Campo 1", "text");
+    $field1 = new SimpleField("nombre", "text", 'Escribe el nombre');
     $form1 -> add($field1);
-    $form1 -> add(new Field("Campo 2", "number"));
-    $form1 -> add(new Field("Campo 3", "color"));
-    $form1 -> add(new Field("Check 4", "color"));
+    $form1 -> add(new SimpleField('edad', 'number', '¿Qué edad tienes?', '18'));
+    $form1 -> add(new SimpleField('color', 'color', '¿Cuál es tu color favorito?', '#FF0000'));
+    $form1 -> add(new SimpleField('suscripcion', 'checkbox', '¿quieres suscribirte?', 'true'));
 ?>
 <!DOCTYPE html>
 <html lang="es">
